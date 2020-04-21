65f95001f9db3f835e4d1ead964f8fe8d41948a5
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-21 10:58:24 +0530

    Added: Welcome message to Census Analyser Problem.

---
+++ CensusAnalyserProblem/CensusAnalyserMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyserProblem
+{
+    class CensusAnalyserMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Census Analyser Problem !!");
+        }
+    }
+}
